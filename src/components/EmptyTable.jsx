87da2e12451e5544d7528a6e3f8deadd1fbc3312
rakaiskandar <iskandarraka4@gmail.com>
2022-11-10 21:30:19 +0700

    add component for table

---
+++ src/components/EmptyTable.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const EmptyTable = ({columns}) => {
+    return ( 
+        <>
+            <table className="my-4 border-b-[1px] border-gray-300 border-collapse bg-white text-sm w-full text-left">
+                <thead>
+                    <tr>
+                        {columns.map((col, i) => (
+                            <th className="font-semibold rounded-sm p-2" key={i}>
+                                {col.Header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+            </table>
+            <p className="text-sm text-center text-gray-600">Data not exist</p>
+        </>
+     );
+}
+ 
+export default EmptyTable;
