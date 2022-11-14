cf448af17068f8d9cbc9347683f70501ba09c96f
rakaiskandar <iskandarraka4@gmail.com>
2022-11-14 20:52:39 +0700

    add recoil

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,33 @@ import Login from "./pages/Login"
 import NewRooms from "./pages/app/rooms/NewRooms"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { RecoilRoot } from "recoil"
 
 function App() {
 
   return (
     <HelmetProvider>
-      <ToastContainer/>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="app" element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path="home" element={<Dashboard/>}/>
-          <Route path="user" element={<User/>}/>
-          <Route path="user/new" element={<NewUser/>}/>
-          <Route path="class" element={<ClassGrade/>}/>
-          <Route path="class/new" element={<NewClass/>}/>
-          <Route path="schedule" element={<Schedule/>}/>
-          <Route path="schedule/new" element={<NewSchedule/>}/>
-          <Route path="room" element={<Rooms/>}/>
-          <Route path="room/new" element={<NewRooms/>}/>
-          <Route/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <RecoilRoot>
+        <ToastContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="app" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="home" element={<Dashboard />} />
+              <Route path="user" element={<User />} />
+              <Route path="user/new" element={<NewUser />} />
+              <Route path="class" element={<ClassGrade />} />
+              <Route path="class/new" element={<NewClass />} />
+              <Route path="schedule" element={<Schedule />} />
+              <Route path="schedule/new" element={<NewSchedule />} />
+              <Route path="room" element={<Rooms />} />
+              <Route path="room/new" element={<NewRooms />} />
+              <Route />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
     </HelmetProvider>
   )
 }
