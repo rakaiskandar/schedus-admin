713a5ad05b06a2c9695026b40ade1925a0df43d5
rakaiskandar <iskandarraka4@gmail.com>
2022-11-16 10:38:39 +0700

    update dynamic value from firebase

--- src/pages/app/Dashboard.jsx
+++ src/pages/app/Dashboard.jsx
@@ -4,26 +4,52 @@ import icon1 from '../../assets/user.png';
 import icon2 from '../../assets/class.png';
 import icon3 from '../../assets/rooms.png';
 import icon4 from '../../assets/building.png';
-import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "../../atoms/userAtom";
 import { useEffect } from "react";
+import { useState } from "react";
+import { collection, getDoc, getDocs, query, where } from "firebase/firestore";
+import { firestoreDb } from "../../../firebase";
 
 const Dashboard = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
     const user = useRecoilValue(userState);
+    const [loading, setLoading] = useState(false);
+    const [userLength, setUserLength] = useState(null);
+    const [classLength, setClassLength] = useState(null);
+
+    useEffect(() => {
+        getUserLength();
+        getClassLength();
+    }, []);
+
+    const getUserLength = async () => {
+        const q = query(collection(firestoreDb, "users"));
+        const docSnap = await getDocs(q);
+        setUserLength(docSnap.docs.length ? docSnap.docs.length: 0);
+    };
+
+    const getClassLength = async () => {
+        const q = query(collection(firestoreDb, "classgrade"));
+        const docSnap = await getDocs(q);
+        const data = docSnap.docs.map((doc)=>{
+            return doc.data().classname.length
+        }).reduce((total, val)=>{
+            return total + val
+        })
+
+        setClassLength(data ? data: 0);
+    };
 
     const cardItem = [
         {
             image: icon1,
             title: "Total User:",
-            total: 100
+            total: userLength
         },
         {
             image: icon2,
             title: "Total Class:",
-            total: 100
+            total: classLength
         },
         {
             image: icon3,
@@ -56,7 +82,7 @@ const Dashboard = () => {
                             <img src={item.image} alt="card image" className="h-14"/>
                             <div className="flex flex-col">
                                 <p className="text-md text-gray-400 font-medium">{item.title}</p>
-                                <h3 className="text-4xl font-semibold pt-1">{item.total}</h3>
+                                <h3 className="text-4xl font-semibold pt-1">{`${item.total ? item.total : "..."}`}</h3>
                             </div>
                         </div>
                     </div>
