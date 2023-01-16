2a28846c5304ed8de871570b754175b7a38726c2
rakaiskandar <iskandarraka4@gmail.com>
2023-01-16 14:18:02 +0700

    add and edit firebase data

--- src/pages/app/schedule/EditSchedule.jsx
+++ src/pages/app/schedule/EditSchedule.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
@@ -8,10 +8,17 @@ import { useRecoilValue } from "recoil";
 import { firestoreDb } from "../../../../firebase";
 import { userState } from "../../../atoms/userAtom";
 import NavbarAdmin from "../../../components/NavbarAdmin";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const EditSchedule = () => {
     let { id } = useParams();
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
     const user = useRecoilValue(userState);
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
@@ -39,6 +46,29 @@ const EditSchedule = () => {
         }
     }, [])
 
+    const submitHandler = async (data) => {
+        setLoading(true)
+        const id = toast.loading("Edit schedule...")
+        try {
+            await updateDoc(doc(firestoreDb, 'yourschedule', schedule.id), {
+                block: data.block,
+                day: data.day,
+                grade: data.grade,
+                subjectHour1til2: data.hour1til2,
+                subjectHour3til4: data.hour3til4,
+                subjectHour5til6: data.hour5til6,
+                subjectHour7til8: data.hour7til8,
+                subjectHour9til10: data.hour9til10,
+            })
+            toast.update(id, { render: "Edit schedule success", type: "success", isLoading: false, autoClose: 200 })
+        } catch (err) {
+            toast.update(id, { render: "Error!", type: "error", isLoading: false, autoClose: 200 })
+            console.error(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <>
             <Helmet>
@@ -60,7 +90,7 @@ const EditSchedule = () => {
                         <>
                             <h1 className="pageName mb-6">Edit Schedule</h1>
 
-                            <form className="flex flex-col gap-4">
+                            <form className="flex flex-col gap-4" onSubmit={handleSubmit(submitHandler)}>
                                 <div>
                                     <label htmlFor="block" className="font-medium">
                                         Block<span className="text-red-600">*</span>
@@ -69,8 +99,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="block"
                                         className="addInput"
-                                        placeholder="Block schedule" 
-                                        defaultValue={schedule?.block}/>
+                                        placeholder="Block schedule"
+                                        defaultValue={schedule?.block}
+                                        {...register("block", {required: true})}/>
+                                        {errors.block && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                block required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -81,8 +117,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="day"
                                         className="addInput"
-                                        placeholder="Day schedule" 
-                                        defaultValue={schedule?.day}/>
+                                        placeholder="Day schedule"
+                                        defaultValue={schedule?.day} 
+                                        {...register("day", {required: true})}/>
+                                        {errors.day && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                day required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -93,8 +135,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="grade"
                                         className="addInput"
-                                        placeholder="Grade name" 
-                                        defaultValue={schedule?.grade}/>
+                                        placeholder="Grade name"
+                                        defaultValue={schedule?.grade} 
+                                        {...register("grade", {required: true})}/>
+                                        {errors.grade && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                grade required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -105,8 +153,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="subjectHour1til2"
                                         className="addInput"
-                                        placeholder="Subject name in Hour 1 until 2" 
-                                        defaultValue={schedule?.subjectHour1til2}/>
+                                        placeholder="Subject name in Hour 1 until 2"
+                                        defaultValue={schedule?.subjectHour1til2} 
+                                        {...register("hour1til2", {required: true})}/>
+                                        {errors.hour1til2 && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                subject required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -117,8 +171,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="subjectHour3til4"
                                         className="addInput"
-                                        placeholder="Subject name in Hour 3 until 4" 
-                                        defaultValue={schedule?.subjectHour3til4}/>
+                                        placeholder="Subject name in Hour 3 until 4"
+                                        defaultValue={schedule?.subjectHour3til4} 
+                                        {...register("hour3til4", {required: true})}/>
+                                        {errors.hour3til4 && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                subject required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -129,8 +189,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="subjectHour5til6"
                                         className="addInput"
-                                        placeholder="Subject name in Hour 5 until 6" 
-                                        defaultValue={schedule?.subjectHour5til6}/>
+                                        placeholder="Subject name in Hour 5 until 6"
+                                        defaultValue={schedule?.subjectHour5til6} 
+                                        {...register("hour5til6", {required: true})}/>
+                                        {errors.hour5til6 && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                subject required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -141,8 +207,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="subjectHour7til8"
                                         className="addInput"
-                                        placeholder="Subject name in Hour 7 until 8" 
-                                        defaultValue={schedule?.subjectHour7til8}/>
+                                        placeholder="Subject name in Hour 7 until 8"
+                                        defaultValue={schedule?.subjectHour7til8} 
+                                        {...register("hour7til8", {required: true})}/>
+                                        {errors.hour7til8 && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                subject required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div>
@@ -153,8 +225,14 @@ const EditSchedule = () => {
                                         type="text"
                                         id="subjectHour9til10"
                                         className="addInput"
-                                        placeholder="Subject name in Hour 9 until 10" 
-                                        defaultValue={schedule?.subjectHour9til10}/>
+                                        placeholder="Subject name in Hour 9 until 10"
+                                        defaultValue={schedule?.subjectHour9til10} 
+                                        {...register("hour9til10", {required: true})}/>
+                                        {errors.hour9til10 && (
+                                            <span className="text-[13px] ml-1 text-red-500">
+                                                subject required fill
+                                            </span>
+                                        )}
                                 </div>
 
                                 <div className="my-1 justify-end flex gap-3 md:">
@@ -168,7 +246,7 @@ const EditSchedule = () => {
                                         type="submit"
                                         className={`createBtn`}
                                     >
-                                        Create Schedule
+                                        Edit Schedule
                                     </button>
                                 </div>
                             </form>
--- src/pages/app/schedule/NewSchedule.jsx
+++ src/pages/app/schedule/NewSchedule.jsx
@@ -4,10 +4,45 @@ import { Link, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "../../../atoms/userAtom";
 import NavbarAdmin from "../../../components/NavbarAdmin";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { addDoc, collection } from "firebase/firestore";
+import { firestoreDb } from "../../../../firebase";
 
 const NewSchedule = () => {
+    const {
+        register,
+        handleSubmit,
+        formState : { errors }
+    } = useForm();
     const user = useRecoilValue(userState);
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false)
+
+    const submitHandler = async(data) => {
+        setLoading(true)
+        const id = toast.loading("Add schedule...")
+        try{
+            await addDoc(collection(firestoreDb, "yourschedule"), {
+                block: data.block,
+                day: data.day,
+                grade: data.grade,
+                subjectHour1til2: data.hour1til2,
+                subjectHour3til4: data.hour3til4,
+                subjectHour5til6: data.hour5til6,
+                subjectHour7til8: data.hour7til8,
+                subjectHour9til10: data.hour9til10,
+            })
+            toast.update(id, {render: "Add schedule success!", type: "success", isLoading: false, autoClose: 200})
+            navigate("/app/schedule")
+        }catch(err){
+            toast.update(id, {render: "Error!", type: "error", isLoading: false, autoClose: 200})
+            console.log(err)
+        }finally{
+            setLoading(false)
+        }
+    }
 
     return ( 
         <>
@@ -29,7 +64,7 @@ const NewSchedule = () => {
                 <div className="contentContainer">
                     <h1 className="pageName mb-6">New Schedule</h1>
                     
-                    <form className="flex flex-col gap-4">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit(submitHandler)}>
                         <div>
                             <label htmlFor="block" className="font-medium">
                                 Block<span className="text-red-600">*</span>
@@ -38,7 +73,13 @@ const NewSchedule = () => {
                             type="text"
                             id="block" 
                             className="addInput"
-                            placeholder="Block schedule"/>
+                            placeholder="Block schedule"
+                            {...register("block", {required: true})}/>
+                            {errors.block && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    block required fill
+                                </span>
+                            )}
                         </div>
                         
                         <div>
@@ -49,7 +90,13 @@ const NewSchedule = () => {
                             type="text"
                             id="day" 
                             className="addInput"
-                            placeholder="Day schedule"/>
+                            placeholder="Day schedule"
+                            {...register("day", {required: true})}/>
+                            {errors.day && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    day required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -60,7 +107,13 @@ const NewSchedule = () => {
                             type="text"
                             id="grade" 
                             className="addInput"
-                            placeholder="Grade name"/>
+                            placeholder="Grade name"
+                            {...register("grade", {required: true})}/>
+                            {errors.grade && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    grade required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -71,7 +124,13 @@ const NewSchedule = () => {
                             type="text"
                             id="subjectHour1til2" 
                             className="addInput"
-                            placeholder="Subject name in Hour 1 until 2"/>
+                            placeholder="Subject name in Hour 1 until 2"
+                            {...register("hour1til2", {required: true})}/>
+                            {errors.hour1til2 && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    subject required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -82,7 +141,13 @@ const NewSchedule = () => {
                             type="text"
                             id="subjectHour3til4" 
                             className="addInput"
-                            placeholder="Subject name in Hour 3 until 4"/>
+                            placeholder="Subject name in Hour 3 until 4"
+                            {...register("hour3til4", {required: true})}/>
+                            {errors.hour3til4 && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    subject required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -93,7 +158,13 @@ const NewSchedule = () => {
                             type="text"
                             id="subjectHour5til6" 
                             className="addInput"
-                            placeholder="Subject name in Hour 5 until 6"/>
+                            placeholder="Subject name in Hour 5 until 6"
+                            {...register("hour5til6", {required: true})}/>
+                            {errors.hour5til6 && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    subject required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -104,7 +175,13 @@ const NewSchedule = () => {
                             type="text"
                             id="subjectHour7til8" 
                             className="addInput"
-                            placeholder="Subject name in Hour 7 until 8"/>
+                            placeholder="Subject name in Hour 7 until 8"
+                            {...register("hour7til8", {required: true})}/>
+                            {errors.hour7til8 && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    subject required fill
+                                </span>
+                            )}
                         </div>
 
                         <div>
@@ -115,7 +192,13 @@ const NewSchedule = () => {
                             type="text"
                             id="subjectHour9til10" 
                             className="addInput"
-                            placeholder="Subject name in Hour 9 until 10"/>
+                            placeholder="Subject name in Hour 9 until 10"
+                            {...register("hour9til10", {required: true})}/>
+                            {errors.hour9til10 && (
+                                <span className="text-[13px] ml-1 text-red-500">
+                                    subject required fill
+                                </span>
+                            )}
                         </div>
 
                         <div className="my-1 justify-end flex gap-3 md:">
